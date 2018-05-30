f929dd23311f59803f2c85610f5921980181cad0
Kamil Romaszko <kamil.romaszko@gmail.com>
2018-05-30 02:22:31 +0200

    Roxygen documentation + check passing

--- R/modelDown.R
+++ R/modelDown.R
@@ -1,6 +1,53 @@
-library(stringi)
+#' Create page with summary for your models
+#'
+#' @param ... explainers
+#' @param modules selected modules to generate
+#' @param output_folder folder where generated page will be stored
+#'
+#' @export
+#' @import kableExtra
+#' @import whisker
+#' @author Magda Tatarynowicz, Kamil Romaszko, Mateusz Urabński
+#' @examples
+#' \dontrun{
+#' require(ranger)
+#' require(breakDown)
+#' # ranger
+#' HR_ranger_model <- ranger(as.factor(left) ~ .,
+#'                       data = HR_data, num.trees = 500, classification = TRUE, probability = TRUE)
+#' explainer_ranger <- explain(HR_ranger_model,
+#'                       data = HR_data, y = HR_data$left, function(model, data) {
+#'  return(predict(model, data)$prediction[,2])
+#' }, na.rm=TRUE)
+#'
+#' # glm
+#' HR_glm_model <- glm(left~., HR_data, family = "binomial")
+#' explainer_glm <- explain(HR_glm_model, data=HR_data, y = HR_data$left)
+#'
+#' modelDown::modelDown(explainer_ranger, explainer_glm) #all defaults
+#' }
+modelDown <- function(..., modules = c("model_performance", "variable_importance", "variable_response", "prediction_breakdown"),
+                      output_folder="output") {
 
-.getPlotWidth <- function(options, plot_with_variable = NULL, default_width = 800) {
+  args <- list(..., version=1.0 )
+  #named arguments are options (except those specified after ... in function definition)
+  options <- args[names(args) != ""]
+  #unnamed arguments are explainers
+  explainers <- args[names(args) == ""]
+
+  ensureOutputFolderStructureExist(output_folder);
+  do.call(file.remove, list(list.files(output_folder, full.names = TRUE, recursive = TRUE)))
+  copyAssets(system.file("extdata", "template", package = "modelDown"), output_folder)
+
+  generated_modules <- generateModules(modules, output_folder, explainers, options)
+
+  renderModules(generated_modules, output_folder)
+  renderMainPage(generated_modules, output_folder, explainers)
+  utils::browseURL(file.path(output_folder, "index.html"))
+}
+
+
+getPlotWidth <- function(options, plot_with_variable = NULL, default_width = 800) {
   if(!is.null(plot_with_variable)) {
     width <- options[[plot_with_variable]]
   }
@@ -14,31 +61,31 @@ library(stringi)
   width
 }
 
-.makeGeneratorEnviroment <- function() {
+makeGeneratorEnviroment <- function() {
   e <- new.env()
-  e$getPlotWidth <- .getPlotWidth
+  e$getPlotWidth <- getPlotWidth
   e
 }
 
-.copyAssets <- function(from, to) {
+copyAssets <- function(from, to) {
   asset_files <- list.files(from)
   css_files <- asset_files[grepl(".*.css", asset_files)]
   css_files_paths <- lapply(css_files, function(name) {file.path(from, name)})
   file.copy(css_files_paths, to, recursive=TRUE, overwrite = TRUE)
 }
 
-.generateModules <- function(modules_names, output_folder, explainers, options) {
+generateModules <- function(modules_names, output_folder, explainers, options) {
   return(lapply(modules_names, function(module_name) {
     print(paste("Generating ", module_name, "...", sep = ""))
     generator_path <- system.file("extdata", "modules", module_name, "generator.R", package = "modelDown")
-    generator_env <- .makeGeneratorEnviroment()
+    generator_env <- makeGeneratorEnviroment()
     source(generator_path, local=generator_env)
     data <- generator_env$generator(explainers, options, file.path(output_folder, "img"))
     return(data)
   }))
 }
 
-.ensureOutputFolderStructureExist <- function(output_folder) {
+ensureOutputFolderStructureExist <- function(output_folder) {
   if(!dir.exists(output_folder)) {
     dir.create(output_folder)
   }
@@ -49,7 +96,7 @@ library(stringi)
   }
 }
 
-.renderPage <- function(content, modules, output_path) {
+renderPage <- function(content, modules, output_path) {
 
   menu_links <- lapply(modules, function(module) {
     return(list(
@@ -63,29 +110,27 @@ library(stringi)
     menu_links = menu_links
   )
 
-  whisker::iteratelist(data[['menu_links']], name='menu_links')
+  iteratelist(data[['menu_links']], name='menu_links')
 
   base_template_path <- system.file("extdata", "template", "base_template.html", package = "modelDown")
   base_template <- readLines(base_template_path)
-  page <- whisker::whisker.render(base_template, data)
+  page <- whisker.render(base_template, data)
   file.create(output_path)
   writeLines(page, output_path)
 }
 
-.renderModules <- function(modules, output_folder) {
+renderModules <- function(modules, output_folder) {
   lapply(modules, function(module) {
     module_path <- file.path("modules", module[['name']])
     content_template <- readLines(system.file("extdata", module_path, "template.html", package = "modelDown"))
 
-    content <- whisker::whisker.render(content_template, module[['data']])
+    content <- whisker.render(content_template, module[['data']])
     output_path <- file.path(output_folder, paste(module[['name']], ".html", sep=""))
-    .renderPage(content, modules, output_path)
+    renderPage(content, modules, output_path)
   })
 }
 
-.renderMainPage <- function(modules, output_folder, explainers) {
-  library(kableExtra)
-
+renderMainPage <- function(modules, output_folder, explainers) {
   explainers_data <- lapply(explainers, function(explainer){
     list(explainer_name = explainer$label)
   })
@@ -100,28 +145,8 @@ library(stringi)
   )
 
   content_template <- readLines(system.file("extdata", "template", "index_template.html", package = "modelDown"))
-  content <- whisker::whisker.render(content_template, main_page_data)
+  content <- whisker.render(content_template, main_page_data)
   output_path <- file.path(output_folder, "index.html")
-  .renderPage(content, modules, output_path)
-}
-
-modelDown <- function(..., modules = c("model_performance", "variable_importance", "variable_response", "prediction_breakdown"),
-                      output_folder="output") {
-
-  args <- list(..., version=1.0 )
-  #named arguments are options (except those specified after ... in function definition)
-  options <- args[names(args) != ""]
-  #unnamed arguments are explainers
-  explainers <- args[names(args) == ""]
-
-  .ensureOutputFolderStructureExist(output_folder);
-  do.call(file.remove, list(list.files(output_folder, full.names = TRUE, recursive = TRUE)))
-  .copyAssets(system.file("extdata", "template", package = "modelDown"), output_folder)
-
-  generated_modules <- .generateModules(modules, output_folder, explainers, options)
-
-  .renderModules(generated_modules, output_folder)
-  .renderMainPage(generated_modules, output_folder, explainers)
-  browseURL(file.path(output_folder, "index.html"))
+  renderPage(content, modules, output_path)
 }
 
