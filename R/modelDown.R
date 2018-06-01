d96f77189bf2f1e7a527cc4ca2efcaf0c8c18c2b
Magda Tatarynowicz <tatarynowiczm@student.mini.pw.edu.pl>
2018-06-02 01:43:12 +0200

    #9 tables for factor added

--- R/modelDown.R
+++ R/modelDown.R
@@ -160,11 +160,15 @@ renderModules <- function(modules, output_folder) {
 }
 
 renderMainPage <- function(modules, output_folder, explainers) {
-  variables_data <- kable_styling(kable(psych::describe(explainers[[1]]$data)), bootstrap_options = c("responsive", "bordered", "hover"))
+  data_set <- explainers[[1]]$data
+  numeric_columns <- which(sapply(data_set, class) != "factor")
+  factor_columns <- which(sapply(data_set, class) == "factor")
+  variables_data <- kable_styling(kable(psych::describe(data_set[,numeric_columns])), bootstrap_options = c("responsive", "bordered", "hover"))
 
   main_page_data <- list(
     explainers = renderExplainersList(explainers),
     data_summary = variables_data,
+    factor_summary = renderFactorTables(data_set, factor_columns),
     observations_count = nrow(explainers[[1]]$data),
     columns_count = ncol(explainers[[1]]$data)
   )
@@ -182,5 +186,18 @@ renderExplainersList <- function(explainers){
   }
   explainers_ul <- paste(explainers_ul, "</ul>", sep = "")
   explainers_ul
+}
+
+renderFactorTables <- function(data_set, factor_columns){
+  factor_data <- vector()
+  factor_data <- NULL
+
+  for(i in 1:length(factor_columns)){
+    column_number <- factor_columns[[i]]
+    column_name <- names(factor_columns)[i]
+    temp_table <- kable_styling(kable(table(data_set[,column_number]), col.names = c(column_name, "Frequency")), bootstrap_options = c("responsive", "bordered", "hover"), full_width = FALSE)
+    factor_data <- paste(factor_data, temp_table, sep="<br>")
+  }
+  factor_data
 }
 
