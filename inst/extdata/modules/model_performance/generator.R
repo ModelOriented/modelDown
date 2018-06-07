529807537c69d9591a0e614208d8c4f17f2cd102
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-06-07 22:38:53 +0200

    + box plot for model performance

--- inst/extdata/modules/model_performance/generator.R
+++ inst/extdata/modules/model_performance/generator.R
@@ -11,26 +11,36 @@ save_plot_image <- function(file_name, models, options){
 
 make_model_performance_plot_model <- function(explainers, img_folder, options) {
   img_filename <- 'model_performance.png'
+  img_box_filename <- 'model_performance_box.png'
   img_path <- file.path(img_folder, img_filename)
+  img_box_path <- file.path(img_folder, img_box_filename)
 
   models <- lapply(explainers, function(explainer) {
     model_performance(explainer)
   })
 
   file.create(img_path)
-  save_plot_image(img_path, models, options)
+  width <- getPlotWidth(options, "mp.plot_width")
 
-  img_filename
+  pl <- do.call(plot, models)
+  ggsave(img_path, pl, png, width = width, height = 500, limitsize = FALSE)
+  models$geom <- "boxplot"
+  pl_box <- do.call(plot, models)
+  ggsave(img_box_path, pl_box, png, width = width, height = 400, limitsize = FALSE)
+
+  list(img_filename = img_filename,
+       img_box_filename = img_box_filename)
 }
 
 generator <- function(explainers, options, img_folder) {
 
   img_filename <- make_model_performance_plot_model(explainers, img_folder, options)
   list(
-    display_name='Model performance',
+    display_name='Model Performance',
     name='model_performance',
     data=list(
-      img_filename=img_filename
+      img_filename_mp1 = img_filename$img_filename,
+      img_filename_mp2 = img_filename$img_box_filename
     )
   )
 }
