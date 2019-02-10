ac9194580ba3d958562e42c93760c3ac98a412bd
kromash <kamil.romaszko@gmail.com>
2019-02-10 23:07:30 +0100

    Reformat test

--- tests/testthat/test_main_page.R
+++
@@ -1,7 +0,0 @@
-context("Check main page generation")
-
-test_that("object is encrypted and decrypted", {
-  encrypted_object <- encrypt_object(iris, "pass")
-  decrypted_object <- decrypt_object(encrypted_object, "pass")
-  expect_equal(iris, decrypted_object)
-})
