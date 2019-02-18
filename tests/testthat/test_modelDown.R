4e1acd1b574fda096f26c3cdbbeca51559f01c47
kromash <kamil.romaszko@gmail.com>
2019-02-19 00:45:37 +0100

    Fix and make tests run faster

--- tests/testthat/test_modelDown.R
+++ tests/testthat/test_modelDown.R
@@ -5,24 +5,25 @@ test_that("Default arguments", {
     require("ranger")
     require("breakDown")
     require("DALEX")
+    HR_data_subset = head(HR_data, 3000)
 
     # ranger
     HR_ranger_model <- ranger(
       as.factor(left) ~ .,
-      data = HR_data,
+      data = HR_data_subset,
       num.trees = 500,
       classification = TRUE,
       probability = TRUE
     )
     explainer_ranger <- explain(HR_ranger_model,
-                                data = HR_data, y = HR_data$left, function(model, data) {
+                                data = HR_data_subset, y = HR_data_subset$left, function(model, data) {
                                   return(predict(model, data)$prediction[, 2])
                                 }, na.rm = TRUE)
 
     # glm
-    HR_glm_model <- glm(left ~ ., HR_data, family = "binomial")
+    HR_glm_model <- glm(left ~ ., HR_data_subset, family = "binomial")
     explainer_glm <-
-      explain(HR_glm_model, data = HR_data, y = HR_data$left)
+      explain(HR_glm_model, data = HR_data_subset, y = HR_data_subset$left)
 
     modelDown::modelDown(explainer_ranger, explainer_glm,
                          output_folder = "modelDown_tmp")
