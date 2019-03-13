66ebe428e1b6032e27c145d33c0aa06c2f98a281
Magda Tatarynowicz <magda.tatarynowicz@gmail.com>
2019-03-13 23:38:02 +0100

    Archivist and help section added to drift module

--- inst/extdata/modules/drifter/generator.R
+++ inst/extdata/modules/drifter/generator.R
@@ -1,6 +1,9 @@
 library("DALEX")
 library(drifter)
 
+HELP_LINK <- 'https://modeloriented.github.io/drifter/'
+DOCS_LINK <- 'https://modeloriented.github.io/drifter/reference/check_drift.html'
+
 renderModelDriftTables <- function(data_set, factor_columns){
   factor_data <- vector()
   factor_data <- NULL
@@ -39,13 +42,15 @@ generator <- function(explainer_pairs, options, img_folder) {
                          old_explainer$y, new_explainer$y,
                          predict_function = old_explainer$predict_function)
 
+    archivist_link <- save_to_repository(drift, options)
+
     model_drift <- renderDrifterSection("Model Drift", drift$model_drift)
     covariate_drift <- renderDrifterSection("Covariate Drift", data.frame(Variable = drift$covariate_drift$variables , Drift = drift$covariate_drift$drift))
     residual_drift <- renderDrifterSection("Residual Drift", drift$residual_drift)
-
-    drifter_data <- paste0(drifter_data, model_drift, covariate_drift, residual_drift, "</div>")
+    help_section <- paste0("<help-panel help-url='", HELP_LINK, "' docs-url='", DOCS_LINK, "'></help-panel>")
+    archivist_section <- paste0("<div class='archivist-code'><p>Get this object: <code>", archivist_link, "</code></p></div>")
+    drifter_data <- paste0(drifter_data, model_drift, covariate_drift, residual_drift, help_section, "</div>", archivist_section)
   }
-
 
   list(
     display_name='Drifter',
